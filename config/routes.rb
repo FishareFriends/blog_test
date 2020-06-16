233d5b931627048fb69d39020d6ca98528578b03
FishareFriends <charlottearwenbeach@hotmail.com>
2020-06-16 12:22:03 +0100

    Removed WelcomeController and added functionality for bootstrap and some custom css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :articles do
@@ -7,5 +6,6 @@ Rails.application.routes.draw do
   end
 
   root 'pages#home'
+  get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
 end
