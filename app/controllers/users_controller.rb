d24e30d26f1183f3ca7d6401556093dab8ab6755
FishareFriends <charlottearwenbeach@hotmail.com>
2020-06-18 17:12:15 +0100

    Added conditional links for loggedin users and cleaned up users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update]
 
 	def new
 		@user = User.new
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:notice] = "Your account info was updates"
 			redirect_to @user
@@ -19,7 +18,6 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
 		@articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 
@@ -31,6 +29,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:notice] = "Welcome to Test Blog #{@user.username}, you're signed up"
 			redirect_to articles_path
 		else
@@ -43,5 +42,8 @@ class UsersController < ApplicationController
 		params.require(:user).permit(:username, :email, :password)
 	end
 
+	def set_user
+		@user = User.find(params[:id])
+	end
 
 end
